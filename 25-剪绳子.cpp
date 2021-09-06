d460225d45c38b6557e02ce2c99c6922b1253590
lexingsen <lexingsen@163.com>
2021-09-07 02:21:05 +0800

    modify 25

--- 25-剪绳子.cpp
+++ 25-剪绳子.cpp
@@ -9,18 +9,18 @@ public:
         int res = 1;
         if (cnt == 0) {
             for (int i = 1; i <= length / 3; i++) {
-            res *= 3;
+                res *= 3;
             }
         } else if (cnt == 1) {
             length = length - 4;
             for (int i = 1; i <= length / 3; i++) {
-            res *= 3;
+                res *= 3;
             }
             res *= 4;
         } else if (cnt == 2) {
             length = length - 2;
             for (int i = 1; i <= length / 3; i++) {
-            res *= 3;
+                res *= 3;
             }
             res *= 2;
         }
