3551fc9dfe72cb31d9232ece2961a38a19a0408e
lexingsen <lexingsen@163.com>
2021-09-10 10:01:35 +0800

    add 48

---
+++ 48-复杂链表的复制.cpp
@@ -0,0 +1,44 @@
+
+struct ListNode {
+    int val;
+    ListNode* next, *random;
+    ListNode(int x):val(x), next(nullptr), next(random) {}
+};
+
+
+// https://www.acwing.com/solution/content/38525/
+class Solution {
+public:
+    ListNode *copyRandomList(ListNode *head) {
+        if (!head) {
+            return head;
+        }
+        ListNode* p = head;
+        while (p != nullptr) {
+            ListNode* np = new ListNode(p->val);
+            np->next = p->next;
+            p->next = np;
+            p = p->next->next;
+        }
+
+        p = head;
+        while (p != nullptr) {
+            if (p->random != nullptr) {
+                p->next->random = p->random->next;
+            }
+            p = p->next->next;
+        }
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* cur = dummy;
+        p = head;
+        while (p != nullptr) {
+            cur->next = p->next;
+            cur = cur->next;
+            p->next = p->next->next;
+            p = p->next;
+        }
+
+        return dummy->next;
+    }
+};
