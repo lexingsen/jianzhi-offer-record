fe2ba0935ef72a53edb6c2533cdfc555116f78c6
lexingsen <lexingsen@163.com>
2021-09-20 15:04:48 +0800

    add 82

---
+++ 82-圆圈中最后剩下的数字.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lastRemaining(int n, int m){
+        if (n == 1) {
+            return 0;
+        } else {
+            return ((lastRemaining(n-1, m)) + m) % n;
+        }
+    }
+};
