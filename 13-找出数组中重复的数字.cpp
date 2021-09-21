fcf19763a344f4566cf7719c868d4834df83c97d
lexingsen <lexingsen@163.com>
2021-09-21 23:13:38 +0800

    add 13

---
+++ 13-找出数组中重复的数字.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int duplicateInArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < 0 || nums[i] >= n) {
+                return -1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            while (nums[i] != i && nums[nums[i]] != nums[i]) {
+                swap(nums[i], nums[nums[i]]);
+            }
+            if (i != nums[i] && nums[nums[i]] == nums[i]) {
+                return nums[i];
+            }
+
+        }
+        return -1;
+    }
+};
