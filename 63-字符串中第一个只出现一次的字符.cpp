c6fc4c691cb0c734bd3b084518312cc202ab03cb
lexingsen <lexingsen@163.com>
2021-09-21 22:43:21 +0800

    add 63

---
+++ 63-字符串中第一个只出现一次的字符.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    char firstNotRepeatingChar(string s) {
+        char res = '#';
+        unordered_map<char, int> hash;
+        for (auto c: s) {
+            hash[c]++;
+        }
+        for (auto c:s) {
+            if (hash[c] == 1) {
+                return c;
+            }
+        }
+        return res;
+    }
+};。
